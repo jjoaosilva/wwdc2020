a4edb34cd96be59c605455ec568a27469bf35443
Jose Joao <josejoaosilva.n@gmail.com>
2020-05-12 20:17:26 -0300

    create repo

---
+++ Template/PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/Introducao.playgroundpage/main.swift
@@ -0,0 +1,44 @@
+//#-hidden-code
+//
+//  See LICENSE folder for this template’s licensing information.
+//
+//  Abstract:
+//  The Swift file containing the source code edited by the user of this playground book.
+//
+//#-end-hidden-code
+
+/*:
+ 
+ # Why do I love cactus?
+ ## A quick overview of why you will also want to have one.
+ 
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+ 
+ */
+
+//#-hidden-code
+
+import UIKit
+import PlaygroundSupport
+import SpriteKit
+import BookCore
+
+class IntroductionViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    func setup(){
+        let view = SKView()
+        let scene = IntroductionScene()
+        scene.scaleMode = .resizeFill
+        view.presentScene(scene)
+        
+        self.view = view
+    }
+}
+// Present the view controller in the Live View window
+PlaygroundPage.current.liveView = IntroductionViewController()
+
+//#-end-hidden-code
---
+++ Template/PlaygroundBook/Modules/BookCore.playgroundmodule/Sources/IntroductionScene.swift
@@ -0,0 +1,37 @@
+
+import UIKit
+import PlaygroundSupport
+import SpriteKit
+
+public class IntroductionScene: SKScene{
+    
+    var label: SKLabelNode = SKLabelNode()
+    var image: SKSpriteNode = SKSpriteNode(imageNamed: "exportCactos2")
+    var subTitle: SKLabelNode = SKLabelNode()
+    
+    override public func didMove(to view: SKView) {
+        self.backgroundColor = #colorLiteral(red: 0.9764705882352941, green: 0.8509803921568627, blue: 0.5490196078431373, alpha: 1.0)
+        label.text = "Why do I Love Cactus?"
+        subTitle.text = "A quick overview of why you will also want to have one"
+        
+        self.addChild(label)
+        self.addChild(subTitle)
+        self.addChild(image)
+    }
+    override public func didChangeSize(_ oldSize: CGSize) {
+        label.position.x = self.size.width/2
+        label.position.y = self.size.height/2 + 150
+        label.fontSize = 50
+        label.fontName = "\(label.fontName)-Bold"
+        
+        subTitle.position.x = self.size.width/2
+        subTitle.position.y = label.position.y - 50
+        subTitle.fontSize = 25
+        subTitle.fontName = "\(subTitle.fontName)-Bold"
+        
+        image.position.x = self.size.width/2
+        
+        image.position.y = self.size.height/2 - 100
+        
+    }
+}
