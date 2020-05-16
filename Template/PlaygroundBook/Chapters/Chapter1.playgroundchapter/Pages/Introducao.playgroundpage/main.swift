c614ccb5cd0a48155bf695ce7bbdaf867937549a
Jose Joao <josejoaosilva.n@gmail.com>
2020-05-16 19:09:35 -0300

    alteracao de textos

--- Template/PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/Introducao.playgroundpage/main.swift
+++ Template/PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/Introducao.playgroundpage/main.swift
@@ -18,7 +18,7 @@ import PlaygroundSupport
  # Why do I love cacti?
  ## An overview of why you will also love it!
  
-Hello, my name is José João, I study Computer Engineering at the Federal University of Ceará and I am a student at IFCE | Apple Developer Academy Program. In these few months of the program, I feel more and more pleasure in producing my illustrations, feeling the graphite on paper, getting my hands dirty and putting the most random ideas on paper.
+Hello, my name is Jose Joao, I study Computer Engineering at the Federal University of Ceara and I am a student at IFCE | Apple Developer Academy Program. In these few months of the program, I feel more and more pleasure in producing my illustrations, feeling the graphite on paper, getting my hands dirty and putting the most random ideas on paper.
  
 But it's not just graphite that I like to get my hands dirty! Have you ever heard about gardening? So, from the time of school I loved to study botany, how plants grow and adapt. And it was in my always crazy routine that I met the cacti!
  
